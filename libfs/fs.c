aacdc7c78ac843743d0b9c20d7dc8c6cfafa59b3
akashmalode22 <45553815+akashmalode22@users.noreply.github.com>
2020-12-03 12:53:53 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -549,6 +549,8 @@ int fs_write(int fd, void *buf, size_t count) {
 			remaining_to_write = remaining_to_write - BLOCK_SIZE;
 		}
 	}
+	block_write(super_block->ROOT_DIRECTORY_BLOCK, root_directory);
+
 	return count;
 }
 
